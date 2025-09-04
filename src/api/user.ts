b1825e254eecec0c59b51e811198f4f6ce70a36c
fishshi <2855691008@qq.com>
2025-09-04 12:31:57 +0800

    add type for /api/user.ts

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,21 +1,21 @@
 import request from "@/utils/request";
 
 // 用户注册
-export const userRegisterService = (registerData) => {
+export const userRegisterService = (registerData: { username: string; password: string }) => {
   return request.post("/auth/register", registerData, {
     headers: { "Content-Type": "application/json" },
   });
 };
 
 // 用户登录
-export const userLoginService = (loginData) => {
+export const userLoginService = (loginData: { username: string; password: string }) => {
   return request.post("/auth/login", loginData, {
     headers: { "Content-Type": "application/json" },
   });
 };
 
 // 检查用户名是否可注册
-export const canRegisterService = (username) => {
+export const canRegisterService = (username: string) => {
   return request.get("/auth/canRegister", {
     params: { username },
   });
@@ -27,14 +27,14 @@ export const userInfoService = () => {
 };
 
 // 修改个人信息
-export const userInfoUpdateService = (userInfoData) => {
+export const userInfoUpdateService = (userInfoData: { email: string, phone: string }) => {
   return request.put("/user/updateUserInfo", userInfoData, {
     headers: { "Content-Type": "application/json" },
   });
 };
 
 // 修改头像
-export const userAvatarUpdateService = (avatarUrl) => {
+export const userAvatarUpdateService = (avatarUrl: string) => {
   return request.patch(
     "/user/updateAvatar",
     { avatarUrl },
@@ -45,7 +45,7 @@ export const userAvatarUpdateService = (avatarUrl) => {
 };
 
 // 修改密码
-export const userPasswordUpdateService = (oldPassword, newPassword) => {
+export const userPasswordUpdateService = (oldPassword: string, newPassword: string) => {
   return request.patch(
     "/user/updatePassword",
     { oldPassword, newPassword },
